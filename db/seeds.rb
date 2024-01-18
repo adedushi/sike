817897d9f822e29c4bc78e5c6404f2bd467d5eb4
Albert Dedushi <adedushi@hotmail.com>
2024-01-18 14:16:23 -0500

    Display product index with placeholder images

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,6 +83,78 @@ ApplicationRecord.transaction do
     sub_category: "Soccer"
   )
 
+  Product.create!(
+  name: "Nike Alpha Huarache Elite 4 Low MCS",
+  subtitle: "Men's Baseball Cleats",
+  description: "Let the baseball world know what kind of bat-wielding beast you are. The midsole has Nike React technology, perfect for sprinting around the bags or robbing your opponent of extra bases. The plush, breathable upper helps secure your foot without adding any unnecessary weight. Plus, the grippy traction is tailor-made for all-out effort plays.",
+  list_price: 95.00,
+  sale_price: nil, 
+  article_number: "FQ7724-104",
+  division: "Men", 
+  category: "Shoes", 
+  sub_category: "Baseball"
+)
+
+Product.create!(
+  name: "Nike Force Zoom Trout 9 Pro",
+  subtitle: "Baseball Cleats",
+  description: "You want to be great? Go do it all on the field, fast, with force, just like Mike Trout. The Trout 9 is lighter, stronger and more comfortable than the previous iteration. Most of all? It's now a low-top design, harkening back to Trout’s favorite cleat as a rookie. With a brand-new upper and a revamped midsole, it can help you leave a permanent imprint on the game. What’s your legacy going to be?",
+  list_price: 90.00,
+  sale_price: nil,
+  article_number: "FB2907-100",
+  division: "Men",
+  category: "Shoes",
+  sub_category: "Baseball"
+)
+
+Product.create!(
+  name: "Jordan ADG 4 x Eastside Golf",
+  subtitle: "Men's Golf Shoes",
+  description: "A request from MJ himself, the Jordan 4 ADG infuses a classic shoe with retro Jordan flavor. Soft full-grain leather shows off the iconic elephant print on the heel. The rubber sole has a traction pattern that pairs the grip you need with the versatility and comfort you want. This collaboration with Eastside Golf celebrates the love of both games.",
+  list_price: 205.00,
+  sale_price: nil,
+  article_number: "FJ0850-001",
+  division: "Men",
+  category: "Shoes",
+  sub_category: "Golf"
+)
+
+Product.create!(
+  name: "Nike In-Season TR 13",
+  subtitle: "Men's Workout Shoes",
+  description: "When time is of the essence, but your workout can’t wait any longer, ride with the Nike In-Season TR 13. It offers versatility, the kind that can carry you from a hard-hitting HIIT class to the treadmill and turn a squeezed-in gym session into sweat-soaked satisfaction. Whether you’re moving laterally or deepening your cardio base by reeling in a few miles in front of you, the energy return helps you reap what you sow.",
+  list_price: 85.00,
+  sale_price: 67.97,
+  article_number: "DZ9360-001",
+  division: "Men",
+  category: " Shoes",
+  sub_category: "Workouts"
+)
+
+Product.create!(
+  name: "Nike Dunk Low",
+  subtitle: "Women's Shoes",
+  description: "Created for the hardwood but taken to the streets, the '80s b-ball icon returns with perfectly shined overlays and classic team colors. With its iconic hoops design, the Nike Dunk Low channels '80s vintage back onto the streets while its padded, low-cut collar lets you take your game anywhere—in comfort.",
+  list_price: 115.00,
+  sale_price: nil,
+  article_number: "DD1503-101",
+  division: "Women",
+  category: "Shoes",
+  sub_category: "Lifestyle"
+)
+
+Product.create!(
+  name: "Nike Air Max SC",
+  subtitle: "Women's Shoes",
+  description: "With its easy going lines, heritage track look and of course, visible Air cushioning, the Nike Air Max SC is the perfect finish to any outfit. The rich mixture of materials adds depth while making it a durable and lightweight shoe for everyday wear.",
+  list_price: 90.00,
+  sale_price: nil,
+  article_number: "CW4554-500",
+  division: "Women",
+  category: "Shoes",
+  sub_category: "Lifestyle"
+)
+
   #   Product.create!(
   #   name: 
   #   subtitle: 
--- frontend/src/components/product_index/ProductIndex.jsx
+++ frontend/src/components/product_index/ProductIndex.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { fetchProducts } from "../../store/products";
 import { useSearchParams } from "react-router-dom"
+import './ProductIndex.css'
 
 const ProductIndex = () => {
     const dispatch = useDispatch();
@@ -17,25 +18,38 @@ const ProductIndex = () => {
     //     return <div>Error: {error.status} {error.statusText}</div>;
     // }
 
+    const USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0
+    });
+
     if (!products) {
         return <div>Loading...</div>;
     }
 
     return (
-        <ul>
-            {products.map((product) => (
-            <>
-            <li key={product.id}> {product.name} </li>
-            <li key={product.id}> {product.subtitle} </li>
-            <li key={product.id}> {product.listPrice} </li>
-            <br/>
-            </>
-            ))}
-            {/* <h1>{product.name}</h1>
-            <h1>{product.subtitle}</h1>
-            <h1>{product.listPrice}</h1>
-            <h1>{product.description}</h1> */}
-        </ul>
+    <div className="product-page">
+        <aside className="filter-sidebar">
+            <h2>Lifestyle Shoes</h2>
+            <h2>Division</h2>
+        </aside>
+
+        <section className="product-grid">
+                {products.map((product) => (
+                    <article key={product.id} className="product-item">
+                        <img src="../public/crocs.jpg" alt="Nike Air Force 1 '07"></img>
+                        <h3>Best Seller</h3>
+                        <p> {product.name} </p>
+                        <p> {product.subtitle} </p>
+                        <h3 > {USDollar.format(product.listPrice)} </h3>
+                        <h3 > {USDollar.format(product.salePrice)} </h3>
+                        <br />
+                    </article>
+                ))}
+            
+        </section>
+    </div>
     );
 }
 
