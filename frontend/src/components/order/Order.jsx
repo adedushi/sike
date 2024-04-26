0b1fa49c102ec3897f31d17c33a282af2a69ce36
Albert Dedushi <adedushi@hotmail.com>
2024-04-25 23:14:39 -0400

    Add styling for orders

--- frontend/src/components/order/Order.jsx
+++ frontend/src/components/order/Order.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './Order.css'
-import { Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchOrder } from '../../store/orders';
 import logo from '../nav_bar/logo.svg'
@@ -35,7 +35,6 @@ const Order = () => {
     const selectOrderItemsArray = createSelector(orderItemsSelector, 
         orderItems => Object.values(orderItems)
     )
-
     const orderItems = useSelector(selectOrderItemsArray)
 
     if (!sessionUser) return <Navigate to="/session" replace={true} />;
@@ -52,14 +51,62 @@ const Order = () => {
         )
     }
 
+    const USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }
+    );
+
     return (
-        <>
-        <h1>{subtotal}</h1>
-        <h1>{shipping}</h1>
-        <h1>{total}</h1>
-        {orderItems.length ? orderItems.map((item) => (
-            <h1 key={item.id}>{item.name}</h1>)) : null}
-        </>
+        <div className='orders-container'>
+            <div className='orders-information'>
+                <div className='orders-left-column'>
+                    <Link className="orders-header"to={'/orders'} >
+                        <h1 >Orders</h1>
+                    </Link>
+                    <div className='orders-order-items'>
+                        {orderItems.length ? orderItems.map((item) => (
+                            <div key={item.id} className='orders-order-item'>
+                                <Link to={`/products/${item.productId}`} className="orders-order-item-link">
+                                    <img src={Array.isArray(item.photosUrl) ? item.photosUrl[0] : null} alt={item.name} className="orders-order-item-image" />
+                                </Link>
+                                <div className="orders-order-item-details">
+                                    <Link to={`/products/${item.productId}`} className="orders-order-item-link">
+                                        <h2 className="orders-order-item-title">{item.name}</h2>
+                                    </Link>
+                                    <div className='orders-order-item-information'>
+                                        <h2 className="orders-order-item-price">{USDollar.format(item.price)}</h2>
+                                        <h3>{item.subtitle}</h3>
+                                        <h3>Size {item.size}</h3>
+                                        <h3>Style {item.articleNumber}</h3>
+                                        {item.quantity > 1 ?
+                                            <h3>Quantity: {item.quantity}</h3> : null}
+                                    </div>
+                                </div>
+                            </div>
+                        )) : null}
+                    </div>
+                </div>
+                <div className='orders-right-column'>
+                    <h2 className="orders-header">Summary</h2>
+                        <div className='orders-cost'>
+                            <div className='orders-line-item'>
+                                <span>Subtotal</span>
+                                <span>{USDollar.format(subtotal)}</span>
+                            </div>
+                            <div className='orders-line-item'> 
+                                <span>Shipping</span>
+                                <span>{USDollar.format(shipping)}</span>
+                            </div>
+                            <div className='orders-line-item-total' > 
+                                <span>Total</span>
+                                <span>{USDollar.format(total)}</span>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
