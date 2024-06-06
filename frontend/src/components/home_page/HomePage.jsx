3798c38b7d53771f7f9251f069134980bae5c885
Albert Dedushi <adedushi@hotmail.com>
2024-06-05 23:04:04 -0400

    Create a preliminary carousel

--- frontend/src/components/home_page/HomePage.jsx
+++ frontend/src/components/home_page/HomePage.jsx
@@ -1,10 +1,13 @@
 import CampaignDisplay from '../campaign_display/CampaignDisplay';
-
+import ProductCarousel from '../product_carousel/ProductCarousel';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
         <>
             <CampaignDisplay />
+            <div className="homepage-spacer"/>
+            <ProductCarousel />
         </>
     )
 }
---
+++ frontend/src/components/product_carousel/ProductCarousel.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { fetchProducts } from '../../store/products';
+import { createSelector } from 'reselect';
+import './ProductCarousel.css';
+import { Link } from 'react-router-dom';
+
+const ProductCarousel = () => {
+    const dispatch = useDispatch();
+    const productsSelector = state => state.products.products;
+    const selectProductsArray = createSelector(productsSelector, (products) => Object.values(products || {}));
+    const products = useSelector(selectProductsArray);
+
+    useEffect(() => {
+        dispatch(fetchProducts({}, 1, 10))
+    }, [dispatch])
+
+    return (
+        <Swiper
+            modules={[Navigation]}
+            // spaceBetween={10}  
+            navigation
+            loop={true}
+            breakpoints={{
+                1600: { slidesPerView: 8 },
+                0: { slidesPerView: 5}
+            }}
+        >
+            {products.map(product => (
+                <SwiperSlide key={product.id}>
+                    <Link to={`/products/${product.id}`} className="carousel-item-link">
+                        <img src={product.photosUrl ? product.photosUrl[0] : null} alt={product.name}></img>
+                        <h3>{product.name}</h3>
+                    </Link>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
+
+export default ProductCarousel;
